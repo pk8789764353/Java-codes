9554ac7e9d0fa0fe476cbc75bf9d15b625a404c0
pk8789764353 <priyanshukumar5104@gmail.com>
2025-01-30 00:27:46 +0530

    i

---
+++ hashmap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class hashmap
+{
+    public static void main(String args[])
+    {
+        HashMap<String,Integer> map =new HashMap<>();
+        map.put("India",100);
+        map.put("china",100);
+        map.put("japan",100);
+        map.put("bhutan",100);
+        map.put("srilanka",100);
+        System.out.println(map);
+
+
+    }
+}
